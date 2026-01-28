210f7890433f834155b6aef8d1096e2ce99132ef
Ivan <73405951+Ivaan2@users.noreply.github.com>
2026-01-28 15:43:52 +0100

    fix firebase project

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,9 +7,9 @@ const firebaseConfig = {
   apiKey: "AIzaSyB5yNUUMEhRv1YDQfOopl2a_khb2G0CRSU",
   authDomain: "studio-4537290699-7858b.firebaseapp.com",
   projectId: "studio-4537290699-7858b",
-  storageBucket: "studio-4537290699-7858b.appspot.com",
+  storageBucket: "studio-4537290699-7858b.firebasestorage.app",
   messagingSenderId: "941138373143",
-  appId: "1:941138373143:web:60e53b2bcd2746d9820a52",
+  appId: "1:941138373143:web:60e53b2bcd2746d9820a52"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
